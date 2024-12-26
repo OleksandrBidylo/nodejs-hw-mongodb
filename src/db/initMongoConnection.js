8f68d6902bac16a3076c84db35a7251a9bfa2de0
OleksandrBidylo <161107668+OleksandrBidylo@users.noreply.github.com>
2024-12-26 13:01:38 +0200

    1

--- src/db/initMongoConnection.js
+++ src/db/initMongoConnection.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 const { MONGODB_USER, MONGODB_PASSWORD, MONGODB_URL, MONGODB_DB } = process.env;
-
 const uri = `mongodb+srv://${MONGODB_USER}:${MONGODB_PASSWORD}@${MONGODB_URL}/${MONGODB_DB}?retryWrites=true&w=majority`;
 
 async function initMongoConnection() {
--- src/models/contact.js
+++ src/models/contact.js
@@ -16,5 +16,4 @@ const contactSchema = new mongoose.Schema(
 );
 
 const Contact = mongoose.model('Contact', contactSchema);
-
 module.exports = Contact;
