276c55974b0e6e60799765243ae5ea020c4a6824
OleksandrBidylo <161107668+OleksandrBidylo@users.noreply.github.com>
2025-01-03 04:06:00 +0200

    1

--- src/server.js
+++ src/server.js
@@ -1,6 +1,9 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 import authRouter from './routers/auth.js';
+
+dotenv.config();
 
 function setupServer() {
   const app = express();
@@ -14,23 +17,25 @@ function setupServer() {
     res.status(status).json({ message: err.message });
   });
 
+  const mongoUri = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_URL}/${process.env.MONGODB_DB}?retryWrites=true&w=majority`;
+
+  mongoose
+    .connect(mongoUri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => {
+      console.log('Database connected');
+      const PORT = process.env.PORT || 3002;
+      app.listen(PORT, () =>
+        console.log(`Server running on http://localhost:${PORT}`),
+      );
+    })
+    .catch((err) => {
+      console.error('Database connection error:', err.message);
+    });
+
   return app;
-}
-
-export async function startServer() {
-  const app = setupServer();
-
-  try {
-    await mongoose.connect('mongodb://localhost:27017/mydatabase');
-    console.log('Database connected');
-
-    const PORT = process.env.PORT || 3000;
-    app.listen(PORT, () =>
-      console.log(`Server running on http://localhost:${PORT}`),
-    );
-  } catch (err) {
-    console.error('Database connection error:', err.message);
-  }
 }
 
 export default setupServer;
